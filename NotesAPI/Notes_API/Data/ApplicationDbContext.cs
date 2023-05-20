b28acfd68bf952338cb40243cab00e8281776793
Farhad10Abdi <Farhad12fcb@gmail.com>
2023-05-20 22:31:55 +0330

    Add the Project and Note Table to the Database

---
+++ NotesAPI/Notes_API/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Notes_API.Models;
+
+namespace Notes_API.Data
+{
+    public class ApplicationDbContext : DbContext 
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        
+        public DbSet<Notes> Notes { get; set; }
+    }
+}
---
+++ NotesAPI/Notes_API/Models/Notes.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Notes_API.Models
+{
+    public class Notes
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Text { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
