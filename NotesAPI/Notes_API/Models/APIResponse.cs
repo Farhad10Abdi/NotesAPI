6d05b79297f4098e443744f00572d98f14c5edd1
Farhad10Abdi <Farhad12fcb@gmail.com>
2023-05-21 22:23:53 +0330

    Change the order of the APIResponse properties to look cleaner

--- NotesAPI/Notes_API/Models/APIResponse.cs
+++ NotesAPI/Notes_API/Models/APIResponse.cs
@@ -5,8 +5,9 @@ namespace Notes_API.Models
     public class APIResponse
     {
         public bool IsSuccess { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
         public List<string> ErrorMessages { get; set; } = new List<string>();
         public Object result { get; set; }
-        public HttpStatusCode StatusCode { get; set; }
+        
     }
 }
