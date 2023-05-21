894b26507c9b3b5838c60b7e0dca6b57981d9efe
Farhad10Abdi <Farhad12fcb@gmail.com>
2023-05-21 21:23:31 +0330

    Add the NoteRepository

---
+++ NotesAPI/Notes_API/Repository/IRepository/INoteRepository.cs
@@ -0,0 +1,9 @@
+﻿using Notes_API.Models;
+
+namespace Notes_API.Repository.IRepository
+{
+    public interface INoteRepository : IRepository<Notes>
+    {
+        Task UpdateAsync(Notes notes);
+    }
+}
