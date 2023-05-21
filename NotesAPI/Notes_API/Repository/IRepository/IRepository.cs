5c898461977a1fd6b2a326aa6e739c6c91cfd40d
Farhad10Abdi <Farhad12fcb@gmail.com>
2023-05-21 21:06:35 +0330

    Change the return type of GetAllAsync to return list

--- NotesAPI/Notes_API/Repository/IRepository/IRepository.cs
+++ NotesAPI/Notes_API/Repository/IRepository/IRepository.cs
@@ -4,7 +4,7 @@ namespace Notes_API.Repository.IRepository
 {
     public interface IRepository<T> where T : class
     {
-        Task<T> GetAllAsync(Expression<Func<T, bool>>? filter = null);
+        Task<List<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null);
         Task<T> GetAsync(Expression<Func<T, bool>>? filter = null, bool tracked = true);
         Task CreateAsync(T entity);
         Task RemoveAsync(T entity);
