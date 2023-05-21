b868a47b17b28d62ca46434b4775aa548d256d33
Farhad10Abdi <Farhad12fcb@gmail.com>
2023-05-21 19:21:19 +0330

    Adding NoteDTOs

---
+++ NotesAPI/Notes_API/Models/Dto/NoteCreateDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Notes_API.Models.Dto
+{
+    public class NoteCreateDTO
+    {
+        public string Text { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
---
+++ NotesAPI/Notes_API/Models/Dto/NoteDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Notes_API.Models.Dto
+{
+    public class NoteDTO
+    {
+        public int ID { get; set; }
+        public string Text { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
