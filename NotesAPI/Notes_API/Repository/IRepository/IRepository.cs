164ab96908677dcb2f2aca7e7a88cc4bd4da6b28
Farhad10Abdi <Farhad12fcb@gmail.com>
2023-05-21 19:56:01 +0330

    Editing Remove parameter to be an entity not an id

--- NotesAPI/Notes_API/Repository/IRepository/IRepository.cs
+++ NotesAPI/Notes_API/Repository/IRepository/IRepository.cs
@@ -7,7 +7,7 @@ namespace Notes_API.Repository.IRepository
         Task<T> GetAllAsync(Expression<Func<T, bool>>? filter = null);
         Task<T> GetAsync(Expression<Func<T, bool>>? filter = null, bool tracked = true);
         Task CreateAsync(T entity);
-        Task RemoveAsync(int id);
+        Task RemoveAsync(T entity);
         Task SaveChangesAsync();
     }
 }
