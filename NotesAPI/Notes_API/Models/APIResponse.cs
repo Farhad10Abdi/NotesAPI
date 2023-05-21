13757b24216436933240c39913d69018b021abfc
Farhad10Abdi <Farhad12fcb@gmail.com>
2023-05-21 19:28:28 +0330

    Adding APIResponse model

---
+++ NotesAPI/Notes_API/Models/APIResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+
+namespace Notes_API.Models
+{
+    public class APIResponse
+    {
+        public bool IsSuccess { get; set; }
+        public List<string> ErrorMessages { get; set; } = new List<string>();
+        public Object result { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
+    }
+}
