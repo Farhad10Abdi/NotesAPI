12ddee309d30c31756bb80d07733996d2253cd01
Farhad10Abdi <Farhad12fcb@gmail.com>
2023-05-21 19:41:01 +0330

    Adding the interface of the generic respository

---
+++ NotesAPI/Notes_API/Repository/IRepository/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace Notes_API.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T> GetAllAsync(Expression<Func<T, bool>>? filter = null);
+        Task<T> GetAsync(Expression<Func<T, bool>>? filter = null, bool tracked = true);
+        Task CreateAsync(T entity);
+        Task RemoveAsync(int id);
+        Task SaveChangesAsync();
+    }
+}
