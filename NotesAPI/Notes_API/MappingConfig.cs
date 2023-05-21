529990857e66fd30cd7be89444f2999513694583
Farhad10Abdi <Farhad12fcb@gmail.com>
2023-05-21 21:56:10 +0330

    Add Automapper and map Note and NoteDTO

---
+++ NotesAPI/Notes_API/MappingConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Notes_API.Models;
+using Notes_API.Models.Dto;
+
+namespace Notes_API
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig() 
+        {
+            CreateMap<Notes,NoteDTO>().ReverseMap();
+        }
+    }
+}
