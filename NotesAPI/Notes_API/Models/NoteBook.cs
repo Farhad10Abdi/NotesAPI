25decabe1e412e2017ef584e9c4b28a1d3f8b155
Farhad10Abdi <Farhad12fcb@gmail.com>
2023-05-26 11:07:30 +0330

    Adding data notation to the model

--- NotesAPI/Notes_API/Models/NoteBook.cs
+++ NotesAPI/Notes_API/Models/NoteBook.cs
@@ -1,8 +1,12 @@
-﻿namespace Notes_API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Notes_API.Models
 {
     public class NoteBook
     {
+        [Required]
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; }
         public string Description { get; set; }
         public DateTime CreatedDate { get; set; }
