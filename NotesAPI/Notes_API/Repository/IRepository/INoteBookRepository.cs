333c6c2b1dba2e9c8a2f19b13c1e65b5445e6ee7
Farhad10Abdi <Farhad12fcb@gmail.com>
2023-05-26 12:07:14 +0330

    Add the interface of the NoteBook Repository

---
+++ NotesAPI/Notes_API/Repository/IRepository/INoteBookRepository.cs
@@ -0,0 +1,9 @@
+﻿using Notes_API.Models;
+
+namespace Notes_API.Repository.IRepository
+{
+    public interface INoteBookRepository : IRepository<NoteBook>
+    {
+        Task<NoteBook> UpdateAsync(NoteBook noteBook);
+    }
+}
