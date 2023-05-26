b22f25fa2c2ea724f4c9fe2db8beb4f0c128763f
Farhad10Abdi <Farhad12fcb@gmail.com>
2023-05-26 12:08:08 +0330

    Implement the NoteBookRepository

---
+++ NotesAPI/Notes_API/Repository/NoteBookRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Query.Internal;
+using Notes_API.Controllers;
+using Notes_API.Data;
+using Notes_API.Models;
+using Notes_API.Repository.IRepository;
+
+namespace Notes_API.Repository
+{
+    public class NoteBookRepository : Repository<NoteBook>, INoteBookRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public NoteBookRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public async Task<NoteBook> UpdateAsync(NoteBook noteBook)
+        {
+            noteBook.UpdatedDate = DateTime.Now;
+            _db.NoteBooks.Update(noteBook);
+            await _db.SaveChangesAsync();
+
+            return noteBook;
+        }
+    }
+}
