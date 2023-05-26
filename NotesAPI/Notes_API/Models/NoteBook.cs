444d02b14e5dfb2c613fdfae69cc011b32005c80
Farhad10Abdi <Farhad12fcb@gmail.com>
2023-05-26 10:54:02 +0330

    Adding the NoteBook class with one-to-many relationship with Notes

---
+++ NotesAPI/Notes_API/Models/NoteBook.cs
@@ -0,0 +1,14 @@
+﻿namespace Notes_API.Models
+{
+    public class NoteBook
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set;}
+
+        public ICollection<Notes> Notes { get; set; } 
+
+    }
+}
