e4076657859d76e19d24c04466193dbe1c4d9a3f
Farhad10Abdi <Farhad12fcb@gmail.com>
2023-06-02 22:14:22 +0330

    add NoteBookId to the noteDTO

--- NotesAPI/Notes_API/Models/Dto/NoteDTO.cs
+++ NotesAPI/Notes_API/Models/Dto/NoteDTO.cs
@@ -5,9 +5,11 @@ namespace Notes_API.Models.Dto
     public class NoteDTO
     {
         public int ID { get; set; }
+        public int NoteBookId { get; set; }
         public string Text { get; set; }
         [Required]
         public string Title { get; set; }
         public string Description { get; set; }
+        
     }
 }
